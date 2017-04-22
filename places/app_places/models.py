27c0be6268e117b42bf9ca0dab51e203aafb13b7
Piotrek Bulik <bulik.piotr@gmail.com>
2017-04-22 19:10:08 +0200

    added model Place, first css, starting page

--- places/app_places/models.py
+++ places/app_places/models.py
@@ -2,19 +2,35 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
-# class Place(models.Model):
-#
-#     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-#                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-#
-#     name = models.CharField(max_length=64)
-#     address = models.TextField()
-#     phone_number = models.CharField(validators=[phone_regex], blank=True)
-#     url = models.URLField()
-#     position_lat = models.DecimalField()
-#     position_lng = models.DecimalField()
-#
-#
+class Place(models.Model):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    address = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=15, validators=[phone_regex], blank=True)
+    url = models.URLField()
+    position_lat = models.FloatField()
+    position_lng = models.FloatField()
+
+    @property
+    def position(self):
+        return "{} | {}".format(self.position_lat, self.position_lng)
+
+    def __str__(self):
+        return "{}, {}, {}, {}, {}, {}".format(self.name, self.address, self.phone_number, self.url, self.position, self.description)
+
+
+
+
+
+
+
+
+
+
+
 # class Event(models.Model):
 #     name = models.CharField(max_length=128)
 #     position_lat = models.DecimalField()
--- places/app_places/views.py
+++ places/app_places/views.py
@@ -1,9 +1,12 @@
 from django.views import View
 from django.shortcuts import render
+from .models import Place
 
 
 class MainView(View):
 
     def get(self, request):
-        return render(request, 'app_places/main.html', {})
+        places = Place.objects.all()
+        ctx = {'places': places}
+        return render(request, 'app_places/main.html', ctx)
 
